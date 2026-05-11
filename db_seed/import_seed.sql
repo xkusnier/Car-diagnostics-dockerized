80bb63a01dbf45d0020465313a63ceecb0500dcb
KusnierJozef <114062398+xkusnier@users.noreply.github.com>
2026-05-11 09:18:34 +0200

    jhk

--- db_seed/import_seed.sql
+++ db_seed/import_seed.sql
@@ -5,7 +5,7 @@ TRUNCATE TABLE dtc_patterns RESTART IDENTITY CASCADE;
 TRUNCATE TABLE dtc_codes_meaning RESTART IDENTITY CASCADE;
 
 \copy dtc_codes_meaning(id, dtc_code, dtc_description) FROM '/tmp/dtc_codes_meaning.csv' WITH (FORMAT csv, HEADER true);
-\copy dtc_patterns(id, name, primary_cause, confidence, source_url) FROM '/tmp/dtc_patterns.csv' WITH (FORMAT csv, HEADER true);
+\copy dtc_patterns(id, name, primary_cause, source_url) FROM '/tmp/dtc_patterns.csv' WITH (FORMAT csv, HEADER true);
 \copy dtc_pattern_links(id, pattern_id, dtc_code) FROM '/tmp/dtc_pattern_links.csv' WITH (FORMAT csv, HEADER true);
 
 SELECT setval(pg_get_serial_sequence('dtc_codes_meaning', 'id'), COALESCE((SELECT MAX(id) FROM dtc_codes_meaning), 1), true);
